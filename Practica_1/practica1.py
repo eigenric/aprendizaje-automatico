079da71f0307d9f4ea9254b2e2bb07a7bdb6a063
Ricardo Ruiz <ricardoruizfdez@gmail.com>
2022-04-03 23:39:59 +0200

    Práctica terminada. Sin Bonus

--- Practica_1/practica1.py
+++ Practica_1/practica1.py
@@ -6,6 +6,7 @@ Nombre Estudiante: Ricardo Ruiz Fernández de Alba
 import itertools
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
 
 np.random.seed(1)
 
@@ -53,12 +54,110 @@ def display_figure(rng_x, rng_y, fun, ws, var1="x", var2="y", fname="f",
 
 #######
 
+def scatter_label_regression(x, y=None, y_labels=None, y_colors=None,
+                             ws_labels=None, ws_colors=None,
+                             size=5, alpha=0.5,
+                             x1_lim=None, x2_lim=None,
+                             xlabel="$x_1$", ylabel="$x_2$",
+                             ax=None,
+                             legend_upper=False):
+    """
+    Gráfico 2D de puntos, a partir de una matriz homogénea.
+    Opcional: Etiquetado por vector y
+    Opcional: Representa recta(s) de regresión a partir de vector de arrays de pesos (ws)
+
+    :param x: matriz de datos homogénea
+    :param y: vector de etiquetas
+    :param y_labels: Diccionario etiqueta -> nombre para la etiqueta
+    :param y_colors: Colores para cada etiqueta
+    :param ws_labels: Diccionario:  w  -> w_label para regresión lineal
+
+    :param ws_colors: Colores para cada array de pesos
+    :param size: Tamaño del punto. 5 por defecto
+    :param alpha: Transparencia del punto. 0.5 por defecto
+
+    :param x1_lim: Tupla mínimo y máximo valor de x1 
+    :param x2_lim: Tupla mínimo y máximo valor de x2
+
+    :param xlabel: Etiqueta del Eje X
+    :param ylabel: Etiqueta del Eje Y 
+    """
+
+    infinite_colors = itertools.cycle(mcolors.TABLEAU_COLORS)
+
+    # Generamos etiquetas y colores por defecto
+    if ws_labels is not None:
+        if ws_colors is None:
+            ws_colors = tuple(itertools.islice(infinite_colors, len(ws)))
+    
+    if y_labels is not None and y_colors is None:
+        y_colors = tuple(itertools.islice(infinite_colors, len(y_labels)))
+    
+    if ax is None:
+        _, ax = plt.subplots()
+
+    # Sin etiquetas
+    if y is None:
+        ax.scatter(x[:, 1], x[:, 2], s=size, alpha=alpha)
+    else: 
+    # Etiquetas con colores
+        for (label, label_name), color in zip(y_labels.items(), y_colors):
+            # Filtramos los elementos de x de la clase label
+            x_cls = x[y == label]
+            ax.scatter(x_cls[:, 1], x_cls[:, 2], s=size, alpha=alpha, color=color,
+                    label=label_name)
+
+    # Fijamos los límites de los ejes para cuadrar con el mínimo y el máximo
+    x1_min, x1_max = minmax(x[:, 1])
+    x2_min, x2_max = minmax(x[:, 2])
+
+    if x1_lim is None:
+        x1_lim = (x1_min, x1_max)
+    if x2_lim is None:
+        x2_lim = (x2_min, x2_max)
+    
+    ax.set_xlim(*x1_lim)
+    ax.set_ylim(*x2_lim)
+    
+    # Opcional: pintar rectas de regresión para cada vector w de ws
+    if ws_labels is not None:
+        # Pintar la línea (hiperplano) que define cada array de pesos
+        for (w_label, w), w_color in zip(ws_labels.items(), ws_colors):
+            x1_ar = np.array([x1_min, x1_max])
+            # Si w0 + w1x1 + w2x2 = 0 => x2 = -1*(w1*x1 + w0)/w2
+            x2_line = -1*(w[1]*x1_ar + w[0]) / w[2]
+
+            if len(ws_labels) > 1:
+                ax.plot(x1_ar, x2_line, color=w_color, label=w_label)
+            else:
+                ax.plot(x1_ar, x2_line, color=w_color)
+    
+    if y_labels is not None or ws_labels is not None:
+        if legend_upper:
+            ax.legend(bbox_to_anchor=(0,1.02,1,0.2), 
+                      loc="lower left", mode="expand", 
+                      borderaxespad=0, ncol=3)
+        else:
+            ax.legend()
+        
+    #x1_ticks = np.arange(x1_min, x1_max, step=x1_tick)
+    #x2_ticks = np.arange(x2_min, x2_max, step=x2_tick)
+
+    ax.set(xlabel=xlabel, ylabel=ylabel)
+
+    plt.show()
+
+def minmax(arr):
+    """Devuelve el mínimo y el máximo de un array"""
+
+    return (np.min(arr), np.max(arr))
+
 print('EJERCICIO SOBRE LA BUSQUEDA ITERATIVA DE OPTIMOS\n')
 
 def gradient_descent(w_ini, lr, grad_fun, fun, epsilon, max_iters,
                      stop_cond, hist=False):
     """
-    Algoritmo de gradiente descendiente para minimizar funciones.
+    Algoritmo de gradiente Descendente para minimizar funciones.
     Implementación para función de dos variables.
     
     :param w_ini: Punto inicial
@@ -138,10 +237,11 @@ w, it = gradient_descent(initial_point, eta, gradE, E, error2get, maxIter,
 print (f"2a) Numero de iteraciones: {it}")
 print (f"2b) Coordenadas obtenidas: ({w[0]}, {w[1]})")
 
-print("Gráfico para visualizar ejercicio 2")
+print("Figura 1.1: Visualización Gradiente Descendente")
 
 ws, it = gradient_descent(initial_point, eta, gradE, E, error2get, maxIter,
                           stop_cond_error, hist=True)
+
                           
 rng_val = 1.5
 x = np.linspace(-rng_val, rng_val, 50)
@@ -185,11 +285,13 @@ ws, it = gradient_descent(initial_point, eta, gradf, f, None, maxIter,
 print (f"3. Numero de iteraciones: {it}")
 print (f"3. Coordenadas obtenidas: ({ws[-1][0]}, {ws[-1][1]})")
 
-print("3a) Gráfico descenso del gradiente para f. η = 0.01")
+print("3a) Figura 1.2: Gráfico descenso del gradiente para f. η = 0.01")
 
 x = np.linspace(-1.5, -0.5, 50)
 y = np.linspace(-0.5, 1.5, 50)
 display_figure(x, y, f, ws)
+
+input("\n--- Pulsar tecla para continuar ---\n")
 
 # η = 0.1
 
@@ -200,11 +302,13 @@ ws2, it = gradient_descent(initial_point, eta, gradf, f, None, maxIter,
 # print (f"3a.2) Numero de iteraciones: {it}")
 # print (f"3a.2) Coordenadas obtenidas: ({ws2[-1][0]}, {ws2[-1][1]})")
 
-print("3a.2) Gráfico descenso del gradiente para f. η = 0.1")
+print("3a.2) Figura 1.3: Gráfico descenso del gradiente para f. η = 0.1")
 
 x = np.linspace(-3, 2, 50)
 y = np.linspace(-1, 1.5, 50)
 display_figure(x, y, f, ws2, alpha=0.4)
+
+input("\n--- Pulsar tecla para continuar ---\n")
 
 # η = 0.005
 
@@ -213,6 +317,9 @@ display_figure(x, y, f, ws2, alpha=0.4)
                         #  stop_cond_maxIter, hist=True)
 
 ## Gráfica de comparación eta 0.1 y 0.01
+
+
+print("Figura 1.4: Variación de f según eta")
 
 eta001_f = np.array([f(x,y) for x,y in ws])
 eta01_f = np.array([f(x,y) for x,y in ws2])
@@ -229,6 +336,9 @@ ax.set_yticks(np.arange(-1, 9))
 ax.set_xticks(np.arange(0, 51, 5))
 ax.legend(loc = "upper right")
 plt.show()
+
+
+input("\n--- Pulsar tecla para continuar ---\n")
 
 ## Extra: Gráfica de comparación eta 0.01 y 0.005
 
@@ -324,9 +434,9 @@ def dMSE(x, y, w):
 
 
 # Gradiente Descendente Estocastico para Regresión Lineal
-def sgd(x, y, lr, epsilon, max_iters, stop_cond, batch_size=32, hist=False):
+def sgd(x, y, lr, epsilon, max_iters, stop_cond, batch_size, hist=False):
     """
-    Algoritmo de gradiente descendiente estocástico (sgd) aplicado
+    Algoritmo de gradiente Descendente estocástico (sgd) aplicado
     a Regresión Lineal.
 
     :param x: matriz de datos de entrada
@@ -365,7 +475,6 @@ def sgd(x, y, lr, epsilon, max_iters, stop_cond, batch_size=32, hist=False):
         # Sólo un mini-batch participa en la adaptación
         w = w - lr*dMSE(x[batch_ids, :], y[batch_ids], w)
         err = MSE(x[batch_ids, :], y[batch_ids], w)
-        if isinstance(err, list): print("UNA LISTA!")
         w_err.append(err)
 
         it += 1
@@ -380,31 +489,19 @@ def sgd(x, y, lr, epsilon, max_iters, stop_cond, batch_size=32, hist=False):
     return (ws, it) if hist else (w, it)
 
 def sgd_maxIter(x, y, lr, max_iters, epsilon=None, batch_size=32, hist=False):
-    """Gradiente descendiente estocástico con nº máximo de iteraciones"""
+    """Gradiente Descendente estocástico con nº máximo de iteraciones"""
 
     stop_cond_maxIter = lambda it, _, __, ___, max_iters: it >= max_iters
 
     return sgd(x, y, lr, epsilon, max_iters, stop_cond_maxIter, batch_size=batch_size, hist=hist)
 
-def sgd_error(x, y, lr, epsilon, max_iters=None, batch_size=32, hist=False):
-    """Gradiente descendiente estocástico con condición de parada por error"""
-
-    # stop_cond_error = lambda _, __, w_err, epsilon, ___: sum(w_err)/len(w_err) <= epsilon
-    def stop_cond_error(it, __, w_err, epsilon, ___): 
-        mean_w_err = sum(w_err)/len(w_err)
-        print(f"it {it}: {mean_w_err}")
-        return mean_w_err <= epsilon
-
-    return sgd(x, y, lr, epsilon, max_iters, stop_cond_error, batch_size=batch_size, hist=hist)
-
 def sgd_maxIter_error(x, y, lr, epsilon, max_iters=50_000, batch_size=32, hist=False):
-    """Gradiente descendiente estocástico con condición de parada por error o nº maximo de iteraciones"""
+    """Gradiente Descendente estocástico con condición de parada por error o nº maximo de iteraciones"""
 
     stop_cond_maxIter_error = lambda it, _, w_err, epsilon, max_iter: it >= max_iters or w_err <= epsilon
 
     return sgd(x, y, lr, epsilon, max_iters, stop_cond_maxIter_error, batch_size=batch_size, hist=hist)
 
-# Pseudoinversa	
 def pseudoinverse(x):
     """Pseudo-inversa (de Moore-Penrose).
     :param x: Matriz de datos de entrada.
@@ -434,39 +531,24 @@ def regresion_pinv(x, y):
 
     return pseudoinverse(x) @ y
 
-def plot_regression(x, y, ws, xlabel, ylabel):
-    """
-    Grafica los puntos junto con las distintas rectas de regresión asociadas
-    que definen los vectores de pesos
-
-    :param x: Matriz de datos de entrada
-    :param y: Vector objetivo
-    :param ws: Vector de pesos 
-    :param xlabel: Nombre eje X
-    :param ylabel: Nombre eje Y
-    """
-
-    pass
-
 # Lectura de los datos de entrenamiento
 x, y = readData('datos/X_train.npy', 'datos/y_train.npy')
 # Lectura de los datos para el test
 x_test, y_test = readData('datos/X_test.npy', 'datos/y_test.npy')
 
 eta = 0.01
-epsilon = 1e-14
 maxit1, maxit2 = 500, 20_000
-ws_sgd, it = sgd_maxIter(x, y, eta, max_iters=maxit1, hist=True)
-#ws_sgd2, it2 = sgd_maxIter(x, y, eta, max_iters=maxit2, hist=True)
-ws_sgd2, it2 = sgd_error(x, y, eta, epsilon, hist=True)
+
+w_sgd, it = sgd_maxIter(x, y, eta, maxit1)
+w_sgd2, it2 = sgd_maxIter(x, y, eta, maxit2)
 w_pinv = regresion_pinv(x, y)
 
-ein_sgd = MSE(x, y, ws_sgd[-1])
-ein_sgd2 = MSE(x, y, ws_sgd2[-1])
+ein_sgd = MSE(x, y, w_sgd)
+ein_sgd2 = MSE(x, y, w_sgd2)
 ein_pinv = MSE(x, y, w_pinv)
 
-eout_sgd = MSE(x_test, y_test, ws_sgd[-1])
-eout_sgd2 = MSE(x_test, y_test, ws_sgd2[-1])
+eout_sgd = MSE(x_test, y_test, w_sgd)
+eout_sgd2 = MSE(x_test, y_test, w_sgd2)
 eout_pinv = MSE(x_test, y_test, w_pinv)
 
 print ('Bondad del resultado para grad. descendente estocastico:\n')
@@ -483,47 +565,27 @@ print("Pseudoinversa")
 print(f"\t Ein: {ein_pinv}")
 print(f"\t Eout: {eout_pinv}")
 
-fig, ax = plt.subplots()
+# Diccionario entre las etiquetas y su significado
+y_labels = {-1: "Número 1", 1 : "Número 5"}
+y_colors = ("blue", "orange")
 
-# Map entre etiquetas y número correspondientes
-label_number_map = {-1: "Número 1", 1 : "Número 5"}
-color_cycle = itertools.cycle(["blue", "orange", "brown", "red", "green"])
+# Diccionario entre los arrays de pesos y su descripcion
+ws_labels = {f"SGD con {maxit1} iteraciones": w_sgd,
+             f"SGD con {maxit2} iteraciones": w_sgd2,
+             "Pseudoinversa":                 w_pinv
+             }
 
-for label, number in label_number_map.items():
-    # Filtramos los elementos de x de la clase label
-    x_cls = x[y == label]
-    ax.scatter(x_cls[:, 1], x_cls[:, 2], s=5, alpha=0.5, color=next(color_cycle),
-               label=number)
+print("Figura 2.1: Regresiones SGD-500, SGD-20000 y Pseodinversa")
 
-# Fijamos los límites de los ejes para cuadrar con el mínimo y el máximo
-minmax = lambda arr: (np.min(arr), np.max(arr))
-x1_min, x1_max = minmax(x[:, 1])
-x2_min, x2_max = minmax(x[:, 2])
-ax.set_xlim(x1_min, x1_max)
-ax.set_ylim(x2_min, x2_max)
-
-pesos = {f"SGD con {maxit1} iteraciones" : ws_sgd[-1],
-         f"SGD con {maxit2} iteraciones" : ws_sgd2[-1],
-          "Pseudoinversa" : w_pinv }
-
-# Pintar la línea (hiperplano) que define cada array de pesos
-for w_label, w in pesos.items():
-    x1_ar = np.array([x1_min, x1_max])
-    # Si w0 + w1x1 + w2x2 = 0 => x2 = -1*(w1*x1 + w0)/w2
-    x2_line = -1*(w[1]*x1_ar + w[0]) / w[2]
-    ax.plot(x1_ar, x2_line, label=w_label)
-
-ax.legend()
-plt.xlabel("Intensidad media")
-plt.ylabel("Simetría")
-print("2-1) Gráfica que compara intensidad media y simetría ")
-plt.show()
+scatter_label_regression(x, y, y_labels, y_colors, ws_labels,
+                         xlabel="Intensidad media",
+                         ylabel="Simetría")
+                        
 
 input("\n--- Pulsar tecla para continuar ---\n")
 
-#Seguir haciendo el ejercicio...
-
 print('Ejercicio 2\n')
+
 # Simula datos en un cuadrado [-size,size]x[-size,size]
 def simula_unif(N, d, size):
 	return np.random.uniform(-size,size,(N,d))
@@ -536,7 +598,175 @@ def sign(x):
 def f(x1, x2):
 	return sign((x1-0.2)**2+x2**2-0.6) 
 
-#Seguir haciendo el ejercicio...
+print("2a) Generar muestra aleatoria")
+
+N = 1000
+muestra = simula_unif(N, 2, 1)
+
+# Matriz homogénea: 1 | x1 | x2
+columna_unos = np.ones((N, 1))
+x = np.hstack((columna_unos, muestra) )
+
+print("Figura 2.2: Muestra aleatoria uniforme 2D")
+scatter_label_regression(x)
+
+input("\n--- Pulsar tecla para continuar ---\n")
+
+# Apartado 2b).
+# Etiquetar los puntos
+
+# Etiquetado de las muestras
+y = np.array([f(x1,x2) for x1,x2 in muestra])
+
+# Generamos ruido, cambiando 10% de las etiquetas
+noise = np.random.choice(1000, size=N // 10, replace=False) 
+y[noise] = -y[noise]
+
+y_labels = {-1: "Etiqueta -1", 1: "Etiqueta 1"}
+
+print("Figura 2.3: Etiquetado de la muestra aleatoria según f con ruido")
+
+scatter_label_regression(x, y, y_labels, y_colors, 
+                        legend_upper=True)
+
+input("\n--- Pulsar tecla para continuar ---\n")
 
 
+## Apartado 2c)
+## Modelo lineal
 
+print("2c) Regresión Lineal en la muestra aleatoria")
+
+# Matriz de inputs 1 | x1 | x2
+eta = 0.01
+maxit = 200
+
+w_sgd, it = sgd_maxIter(x, y, eta, maxit)
+ein_sgd = MSE(x, y, w_sgd)
+
+w_sgd_unif = w_sgd
+
+print(f"Para {maxit} iteraciones: ")
+print(f"\tEin: {ein_sgd}")
+print(f"\tw: {w_sgd}")
+
+ws_labels = {f"SGD con {it} iteraciones": w_sgd}
+scatter_label_regression(x, y, y_labels, y_colors, 
+                         ws_labels, ws_colors=("k"),
+                         legend_upper=True)
+
+input("\n--- Pulsar tecla para continuar ---\n")
+
+## Apartado 2d)
+## Repetición del experimento
+def generar_muestra2D_uniforme(N=1000, no_lineal=False):
+    """Genera muestra 2D uniforme. 
+    Si no_lineal = True. 
+        Devuelve 1 | x1 | x2 | x1x2 | x1**2 | x2**2
+    """
+
+    muestra = simula_unif(N, 2, 1)
+
+    columna_unos = np.ones((N, 1))
+    x = np.hstack((columna_unos, muestra) )
+
+    y = np.array([f(x1,x2) for x1,x2 in muestra])
+    noise = np.random.choice(N, size=N // 10, replace=False) 
+    y[noise] = -y[noise]
+
+    if no_lineal:
+        x1x2 = x[:, 1] * x[:, 2]
+        x1x1 = x[:, 1] ** 2
+        x2x2 = x[:, 2] ** 2
+
+        # Vector columna
+        x1x2.shape = (N, 1)
+        x1x1.shape = (N, 1)
+        x2x2.shape = (N, 1)
+
+        # Agregamos las columnas x1x2, x1**2 , x2**2
+        x = np.append(x, x1x2, axis=1)
+        x = np.append(x, x1x1, axis=1)
+        x = np.append(x, x2x2, axis=1)        
+
+    return x, y
+
+
+eta = 0.01
+maxit = 200
+
+def errores_promedio(rep=1000, eta=0.01, sgd_maxit=200, no_lineal=False):
+    """Calcula los E_in y E_out promedio"""
+    
+    ein_sgd_total = 0
+    eout_sgd_total = 0
+
+    for i in range(rep):
+        x, y = generar_muestra2D_uniforme(no_lineal=no_lineal)
+    
+        w_sgd, it = sgd_maxIter(x, y, eta, maxit)
+        ein_sgd = MSE(x, y, w_sgd)
+        ein_sgd_total += ein_sgd
+
+        x_test, y_test = generar_muestra2D_uniforme(no_lineal=no_lineal)
+        eout_sgd = MSE(x_test, y_test, w_sgd)
+        eout_sgd_total += eout_sgd
+
+    ein_sgd_medio = ein_sgd_total / rep
+    eout_sgd_medio = eout_sgd_total / rep
+
+    return (ein_sgd_medio, eout_sgd_medio)
+
+
+print("\n 2d) Repetición del experimento 1000 veces:")
+print(f"SGD con {maxit} iteraciones en cada repetición: ")
+
+eta = 0.01
+sgd_maxit = 200
+
+ein_sgd_medio, eout_sgd_medio = errores_promedio(rep=1000, eta=eta,
+                                                sgd_maxit=sgd_maxit)
+
+print(f"E_in promedio: {ein_sgd_medio}")
+print(f"E_out promedio: {eout_sgd_medio}")
+
+input("\n--- Pulsar tecla para continuar ---\n")
+
+print("\n Modelo No Lineal ")
+
+# Generamos la curva de contorno
+x, y = generar_muestra2D_uniforme(no_lineal=True)
+
+eta = 0.1
+maxIter = 200
+w, _ = sgd_maxIter(x, y, eta, maxIter)
+ws_labels = {"SGD con 200 iteraciones": w}
+
+fig, ax = plt.subplots()
+x0_min, x0_max = minmax(x[:, 1])
+x1_min, x1_max = minmax(x[:, 2])
+
+s = 0.5
+x_t, y_t = np.meshgrid(np.linspace(x0_min-s, x0_max+s, 200),
+                       np.linspace(x1_min-s, x0_max+s, 200))
+                       
+circ_t = w[0] + w[1]*x_t + w[2]*y_t + w[3]*x_t*y_t + w[4]*x_t*x_t + w[5]*y_t*y_t
+
+plt.contour(x_t, y_t, circ_t, [0])
+scatter_label_regression(x, y, y_labels, 
+                         x1_lim=(-1, 1.25),
+                         ws_labels=ws_labels, 
+                         ws_colors=("k"),
+                         ax = ax,
+                         legend_upper=True)
+
+
+input("\n--- Pulsar tecla para continuar ---\n")
+
+print("\n Repetición del experimento 1000 veces para características no lineales:")
+print(f"SGD con 200 iteraciones en cada repetición: ")
+
+errs_promedio_no_lineal = errores_promedio(no_lineal=True)
+
+print(f"E_in promedio: {errs_promedio_no_lineal[0]}")
+print(f"E_out promedio: {errs_promedio_no_lineal[1]}")
